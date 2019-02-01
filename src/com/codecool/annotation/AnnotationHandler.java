f0c15b88ab39dcac9c700b979861e417a1025fd9
Szajler Indira <szajler.indira@gmail.com>
2019-02-01 14:07:23 +0100

    getPath(), getResponse() added

--- src/com/codecool/annotation/AnnotationHandler.java
+++ src/com/codecool/annotation/AnnotationHandler.java
@@ -1,23 +1,42 @@
 package com.codecool.annotation;
 
+import com.sun.net.httpserver.HttpExchange;
+
 import java.lang.annotation.Annotation;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
-public class MethodAnnotation {
+public class AnnotationHandler {
 
-    public String getPath() throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
-        String path = null;
+    public String getPath() {
+        String path;
         Method[] methods = AnnotationImplementation.class.getDeclaredMethods();
         for (Method method : methods) {
             Annotation annotation = method.getAnnotation(WebRoute.class);
             if (annotation instanceof WebRoute) {
                 WebRoute webRoute = (WebRoute) annotation;
                 path = webRoute.path();
-                System.out.println(path);
-                method.invoke(null);
+                return path;
             }
         }
-        return path;
+        return null;
+    }
+
+    public static String getResponse(HttpExchange t) throws InvocationTargetException, IllegalAccessException {
+        String response = "Something went wrong";
+
+        String path;
+        Method[] methods = AnnotationImplementation.class.getDeclaredMethods();
+        for (Method method : methods) {
+            Annotation annotation = method.getAnnotation(WebRoute.class);
+            if (annotation instanceof WebRoute) {
+                WebRoute webRoute = (WebRoute) annotation;
+                path = webRoute.path();
+                if (t.getRequestURI().toString().equals(path)) {
+                    response = (String) method.invoke(null);
+                }
+            }
+        }
+        return response;
     }
 }
